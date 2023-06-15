eb71382241b1de9b968f2d54f8f74630c7e70723
Hussnain <hussywork@gmail.com>
2023-06-15 19:10:00 +0500

    Cricket Player Reporting System Basic CRUD
    Develop a WebApi based on a DDD Pattern in .Net6
    Develop a Angular CRUD Application to maintain the data with validation in front and backend ( use FluentValidation Syntax in Both)
    The Angular Application also should have a overview with an grid
    The Application should be startable with a docker-compose and checkable

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Forest Fire Reports';
+
+  refresh(): void {
+    window.location.reload();
+}
+}
